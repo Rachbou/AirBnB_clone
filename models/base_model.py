65a81293ff40a031941be2b18ad02ea4f9fc1bf0
Rachid BOULMANI <RachidBoulmanii@gmail.com>
2024-03-23 14:50:10 -0700

    3. BaseModel

---
+++ models/base_model.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+
+"""
+    Defines a class BaseModel.
+"""
+
+from datetime import datetime
+from uuid import uuid4
+
+
+class BaseModel:
+    """Represent a BaseModel."""
+
+    def __init__(self, *args, **kwargs):
+        """Initialize a new BaseModel"""
+
+        self.id = str(uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
+
+    def __str__(self):
+        """Return a string representation of the class"""
+
+        return "[{:s}] ({:s}) {:s}".format(
+            self.__class__.__name__,
+            self.id,
+            str(self.__dict__)
+        )
+
+    def save(self):
+        """Update the date field"""
+
+        self.updated_at = datetime.now()
+
+    def to_dict(self):
+        """Return a dictionary representation of the instance"""
+
+        rep = {
+            "__class__": self.__class__.__name__,
+            "created_at": self.created_at.isoformat(),
+            "updated_at": self.updated_at.isoformat()
+        }
+
+        dict_copy = self.__dict__.copy()
+        dict_copy.update(rep)
+        return (dict_copy)
---
+++ tests/test_models/test_base_model.py
@@ -0,0 +1,88 @@
+#!/usr/bin/python3
+
+"""
+    Defines a class TestBaseModel.
+"""
+
+
+from models.base_model import BaseModel
+import unittest
+import models
+import os
+
+
+class TestBaseModel(unittest.TestCase):
+    """Represent a TestBaseModel."""
+
+    def setUp(self):
+        """SetUp method"""
+
+        self.base_model = BaseModel()
+        self.base_model.data = "Test data"
+
+    def TearDown(self):
+        """TearDown method."""
+
+        del self.base_model
+
+    def test_init_id(self):
+        """Test different id per object created"""
+
+        bm1 = BaseModel()
+        bm2 = BaseModel()
+        self.assertNotEqual(bm1.id, bm2.id)
+
+    def test_id_type(self):
+        """Test the id type from BaseModel"""
+
+        bm1 = BaseModel()
+        self.assertIsInstance(bm1.id, str)
+
+    def test_docstring(self):
+        """Test docstring for the module and the class"""
+
+        self.assertIsNotNone(
+            models.base_model.__doc__,
+            "No docstring in the module"
+        )
+        self.assertIsNotNone(BaseModel.__doc__, "No docstring in the class")
+
+    def test_permissions_file(self):
+        """Test File base_model.py permissions"""
+
+        test_file = os.access("models/base_model.py", os.R_OK)
+        self.assertTrue(test_file, "Read permissions")
+        test_file = os.access("models/base_model.py", os.W_OK)
+        self.assertTrue(test_file, "Write Permissions")
+        test_file = os.access("models/base_model.py", os.X_OK)
+        self.assertTrue(test_file, "Execute permissions")
+
+    def test_mod_to_dict(self):
+        """Test dictionary representation in BaseModel"""
+
+        bm1 = BaseModel()
+        self.assertIsInstance(bm1.to_dict(), dict)
+
+    def test_type_object(self):
+        """Test type object of BaseModel"""
+
+        bm1 = BaseModel()
+        self.assertEqual(
+            str(type(bm1)),
+            "<class 'models.base_model.BaseModel'>")
+        self.assertIsInstance(bm1, BaseModel)
+
+    def test_str_representation(self):
+        """Test str representation of BaseModel"""
+
+        bm1 = BaseModel()
+        str_rep = "[{:s}] ({:s}) {:s}".format(
+            bm1.__class__.__name__,
+            bm1.id,
+            str(bm1.__dict__)
+        )
+        self.assertEqual(str_rep, str(bm1))
+
+
+if __name__ == "__main__":
+    unittest.main()
